eba8e847a5005c5e474a9ff8c812654f8dc32baf
Dutta4202024 <4202024@sps-student.sk.ca>
2019-10-24 14:15:12 -0600

    Update sketch.js

--- Arrays/sketch.js
+++ Arrays/sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let grid = [];
+let grid;
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
