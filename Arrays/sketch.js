cf83572b9f283555510a116cd2e0101d7af38316
Dutta4202024 <4202024@sps-student.sk.ca>
2019-10-24 13:16:05 -0600

    Update sketch.js

--- Arrays/sketch.js
+++ Arrays/sketch.js
@@ -5,28 +5,47 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let redAmount = 0;
-let redChangeAmount = 1;
+let grid = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(220);
+  createCanvas(windowWidth, windowHeight)
+  grid = create2dArr(20, 20);
 }
 
-function draw() {
-  fill(redAmount,0,255);
-  ellipse(mouseX,mouseY,100,100);
-
-  redAmount += redChangeAmount;
-  
-  if (redAmount >= 255) {
-    redChangeAmount *= -1;
+function draw(){
+  background(220)
+  displayGrid(10)
+}
+function displayGrid(theGrid) {
+  for (let y = 0; y < theGrid[0].length; y++) {
+    for (let x = 0; x < theGrid[0].length; x++){
+      if (theGrid[y][x] === 0) {
+        fill(255);
+      }
+      else {
+        fill(0);
+      }
+      let cellSize = width/theGrid[0].length
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
   }
-  if (redAmount <= 0){
-    redChangeAmount *= -1;
+}
 
-  }
-  console.log(redAmount);
-  
-  }
+function create2dArr(cols, rows) {
+  let someArray = [];
 
+  for (let i=0; i < cols; i++) {
+      someArray.push([]);
+      for (let j = 0; j<rows; j++) {
+        if (random(100) < 50) {
+          someArray[i].push(1);
+
+        }
+        else {
+          someArray[i].push(0);
+        }
+    }
+}
+  return someArray;
+
+}
