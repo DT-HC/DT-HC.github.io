c8cf62afa3c041147fb07fe3904ae8fdc996088c
DT-HC <54913222+DT-HC@users.noreply.github.com>
2019-12-06 14:08:41 -0600

    p

--- LOCALSTORAGE/sketch.js
+++ LOCALSTORAGE/sketch.js
@@ -5,11 +5,37 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let numberOfClicks = 0;
+let allTimeHighestClicks;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  if (getItem("highScore")!== null) {
+    allTimeHighestClicks = getItem("highScore");
+  }
+  else {
+    allTimeHighestClicks = 0;
+  }
 }
 
 function draw() {
   background(220);
+
+  textSize(72);
+  
+  fill("black");
+  text( numberOfClicks, 50, 100);
+  
+  fill("green");
+  text(allTimeHighestClicks, 50, 300);
+
+}
+
+function mousePressed() {
+  numberOfClicks++;
+  if (numberOfClicks > allTimeHighestClicks){
+    allTimeHighestClicks = numberOfClicks;
+    storeItem("highScore", allTimeHighestClicks)
+  }
 }
